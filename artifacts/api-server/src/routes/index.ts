ff7d08049ed481697aa554c3c71646b107dfbf88
vvxsgang <Andreyxalzov81@gmail.com>
2026-04-24 10:18:05 +0500

    Update index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
+import leaderboardRouter from "./leaderboard";
 
 const router: IRouter = Router();
-
 router.use(healthRouter);
+router.use(leaderboardRouter);
 
 export default router;
